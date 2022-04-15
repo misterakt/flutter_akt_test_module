67ff801361f62d207a1b74c4273527b6bab7b978
kwangtaek <kwangtaek.ahn@klarna.com>
2022-04-15 19:10:09 +0200

    test1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
+
+/// https://github.com/QuickBlox/quickblox-flutter-samples/blob/master/lib/main.dart
 
 void main() => runApp(MyApp());
 
@@ -15,26 +17,46 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'what\'s your favorite color?',
-      'answer': ['Black', 'Red', 'Green', 'White', 'Yellow']
+      'answer': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ]
     },
     {
-      'questionText': 'what\'s your favorite animal?',
-      'answer': ['Panda', 'Tiger', 'Lion', 'Dog', 'Horse']
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
-      'questionText': 'who\'s your favorite guy?',
-      'answer': ['Max', 'Min', 'Tom', 'Tommy']
-    }
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Mike', 'score': 3},
+        {'text': 'Suji', 'score': 5},
+        {'text': 'Mini', 'score': 10},
+      ],
+    },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
     if (_questionIndex < _questions.length) {
       print("we have more questions!");
+    } else {
+      print("no more questions!");
     }
   }
 
@@ -42,20 +64,16 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Column(
-            children: [
-              Question(
-                _questions[_questionIndex]['questionText'] as String,
-              ),
-              ...(_questions[_questionIndex]['answer'] as List<String>)
-                  .map((answer) {
-                return Answer(_answerQuestion, answer);
-              }).toList()
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions)
+            : Result(),
+      ),
     );
   }
 }
